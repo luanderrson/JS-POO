57a86a0d6744855f75b1aeedf6432bc8a83d50a8
Luanderson <luandersonbms@gmail.com>
2021-03-22 03:00:55 -0300

    JS Orientado a Objeto

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-class Cliente{
-  nome;
-  cpf;
-  agencia;
-  saldo;
-}
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice",88822233309);
 
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
+const contaCorrenteRicardo = new ContaCorrente(1001,cliente1);
+contaCorrenteRicardo.depositar(500);
+const conta2 = new ContaCorrente(102,cliente1);
 
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
-
-console.log(cliente1);
-console.log(cliente2);
+let valor = 200;
+contaCorrenteRicardo.transferir(valor,conta2);
+console.log("Numero de contas: " + ContaCorrente.numeroDeContas);
